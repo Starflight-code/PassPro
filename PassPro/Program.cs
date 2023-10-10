aa79a4858448fcc3517edeb4b23dc290756dbc7f
ben-kobiske <kobiskeb@uwplatt.edu>
2023-10-10 12:16:54 -0500

    Added program functionality, .gitignore updated Added new gitignore files, added initial program functionality. Throws an error upon incorrect user input.

--- PassPro/Program.cs
+++ PassPro/Program.cs
@@ -1,4 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-string j = Console.ReadLine();
-int num = int.Parse(j);
+Console.Write("\nEnter a number: ");
+string? userInput = Console.ReadLine();
+if (userInput == null)
+{
+    userInput = "";
+}
+int userNumber;
+try
+{
+    userNumber = int.Parse(userInput);
+}
+catch
+{
+    throw new Exception("Number is not a number");
+}
+if (userNumber < 0)
+{
+    throw new Exception("Number is negative");
+}
+int outStars;
+for (int i = 0; i < userNumber; i++)
+{
+    outStars = i * 2 + 1;
+    for (int j = 0; j < outStars; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
