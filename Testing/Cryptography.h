f5ee3d8b89cd00bda6d1f7defe244dd1f0a58f03
Evan <Hesselinke@uwplatt.edu>
2023-11-11 13:53:13 -0600

    updated to include catch rather than boost

---
+++ Testing/Cryptography.h
@@ -0,0 +1,14 @@
+//#ifndef CRYPTOGRAPHY_H
+//#define CRYPTOGRAPHY_H
+//
+//class Cryptography {
+//public:
+//    Cryptography(const unsigned char* key);
+//    ~Cryptography();
+//    void encryptAES256(const unsigned char* plaintext, int plaintextLength, unsigned char* ciphertext);
+//
+//private:
+//    const unsigned char* key_;
+//};
+//
+//#endif
---
+++ Testing/DatabaseObject.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "PasswordEntry.cpp"
+
+class DatabaseObject {
+
+public:
+    std::vector<std::string> name;
+    std::vector<std::string> url;
+    std::vector<std::string> password;
+    std::vector<std::string> username;
+    std::vector<std::string> notes;
+    std::vector<std::string> searchableURL;
+
+public:
+    /**
+     * @brief addEntry: adds all data from a password entry object to a set of parallel vectors
+     * @param entry: a PasswordEntry instance
+     * @return void
+     */
+    void addEntry(PasswordEntry entry) {
+        name.push_back(entry.name);
+        url.push_back(entry.url);
+        password.push_back(entry.password);
+        username.push_back(entry.username);
+        notes.push_back(entry.notes);
+        searchableURL.push_back(entry.seachableURL);
+    }
+};
