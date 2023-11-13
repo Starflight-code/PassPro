c0d44c3651c427c2c4e579a386419467bd741b38
Evan <Hesselinke@uwplatt.edu>
2023-11-12 19:02:15 -0600

    Added more tests for Sanitation, encryption, and searchable URL tests. Added some code in DatabaseManager.cpp and PasswordEntry.cpp for easier testing. Also implimented some memory leak tests for PasswordEntry.cpp, DatabaseManager.cpp, and DatabaseObject.cpp

--- Testing/Testing/DatabaseManager.cpp
+++ Testing/Testing/DatabaseManager.cpp
@@ -1,32 +1,46 @@
 #pragma once
 
+#include "DatabaseObject.cpp"
 #include "PasswordEntry.cpp"
+#include "nlohmann/json.hpp"
 #include <iostream>
 #include <vector>
-#include "nlohmann/json.hpp"
-#include "DatabaseObject.cpp"
 
 class DatabaseManager {
-    std::vector<PasswordEntry> entries;
+  private:
+  std::vector<PasswordEntry> entries;
+  friend class DatabaseManagerTest;
 
-    /**
-     * @brief writeDB: Sanitizes current DB state, converts
-     * to JSON, encrypts and pushes it to the disk
-     */
-    void writeDB() {
-        DatabaseObject db;
-        for (int i = 0; i < entries.size(); i++) {
-            db.addEntry(entries[i]);
-        }
-        nlohmann::json j;
-        j["names"] = db.name;
-        j["urls"] = db.url;
-        j["s_urls"] = db.searchableURL;
-        j["notes"] = db.notes;
-        j["usernames"] = db.username;
-        j["passwords"] = db.password;
+  public:
+  const std::vector<PasswordEntry>& getEntries() const {
+    return entries;
+  }
 
-        std::cout << "JSON Data Stored (DEVELOPMENT ONLY / DO NOT LEAVE THIS IN PRODUCTION)";
-        std::cout << nlohmann::json::string_t(j);
+  // Add a method to add entries
+  void addEntry(const PasswordEntry& entry) {
+    entries.push_back(entry);
+  }
+
+  /**
+   * @brief writeDB: Sanitizes current DB state, converts
+   * to JSON, encrypts and pushes it to the disk
+   */
+  void writeDB() {
+    DatabaseObject db;
+    for(int i = 0; i < entries.size(); i++) {
+      db.addEntry(entries[i]);
     }
+    nlohmann::json j;
+    j["names"] = db.name;
+    j["urls"] = db.url;
+    j["s_urls"] = db.searchableURL;
+    j["notes"] = db.notes;
+    j["usernames"] = db.username;
+    j["passwords"] = db.password;
+
+    std::cout << "JSON Data Stored (DEVELOPMENT ONLY / DO NOT LEAVE THIS IN PRODUCTION)";
+    // Comment out line 43 fir testing, uncomment line 44 for implimentation
+    //std::cout << nlohmann::json::string_t(j);
+    std::cout << j.dump();
+  }
 };
