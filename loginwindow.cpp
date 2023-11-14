5c2942b049b2e8725d3a728a538c23e33d44b210
ben-kobiske <kobiskeb@uwplatt.edu>
2023-11-14 11:40:59 -0600

    Adjusted login page, added event handling Added event handling for return key pressing and hitting the submit button. Hid the password display text (****), and adjusted the size of some UI elements. (#5)

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,10 +1,21 @@
 #include "loginwindow.h"
 #include "ui_loginwindow.h"
 
-LoginWindow::LoginWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::LoginWindow) {
+LoginWindow::LoginWindow(QWidget *parent)
+    : QMainWindow(parent), ui(new Ui::LoginWindow) {
   ui->setupUi(this);
 }
 
-LoginWindow::~LoginWindow() {
-  delete ui;
+LoginWindow::~LoginWindow() { delete ui; }
+
+void LoginWindow::on_lineEdit_2_returnPressed() {
+  std::string username = ui->lineEdit->text().toStdString();
+  std::string password = ui->lineEdit_2->text().toStdString();
+  // TODO: Add database unlocking code
+}
+
+void LoginWindow::on_pushButton_clicked() {
+  std::string username = ui->lineEdit->text().toStdString();
+  std::string password = ui->lineEdit_2->text().toStdString();
+  // TODO: Add database unlocking code
 }
--- main.cpp
+++ main.cpp
@@ -1,23 +1,27 @@
 #include "DatabaseManager.cpp"
 #include "include/BS_thread_pool.hpp"
-#include "mainwindow.h"
-#include "testing.cpp"
+// #include "mainwindow.h"
+// #include "testing.cpp"
+#include "loginwindow.h"
 #include <thread>
 
 #include <QApplication>
 
-int main(int argc, char* argv[]) {
-  if(argc == 2 && !strcmp(argv[1], "-test")) {
+int main(int argc, char *argv[]) {
+  /*if(argc == 2 && !strcmp(argv[1], "-test")) {
     std::cout << "Running Test Suite\n";
     Testing::TestSuite testSuite;
     testSuite.runAllTests();
     return 0;
-  }
+  }*/
   BS::thread_pool pool(3); // Pool has 3 threads, increase if required
   DatabaseManager data;
-  // data.writeDB(); // uncomment for JSON DB sanitization/desanitization testing
+  // data.writeDB(); // uncomment for JSON DB sanitization/desanitization
+  // testing
   QApplication a(argc, argv);
-  MainWindow w;
-  w.show();
+  // MainWindow w;
+  LoginWindow log;
+  // w.show();
+  log.show();
   return a.exec();
 }
