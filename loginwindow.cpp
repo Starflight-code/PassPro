78914719f7ea495fbe776c895576c83dd00f20a2
ben-kobiske <kobiskeb@uwplatt.edu>
2023-11-22 12:30:18 -0600

    Removed CryptoPP, re-implimented secure strings, now compiles Removed CryptoPP, the library appears broken and causes compilation errors. Used a custom allocator for secure string implimentation, and used OpenSSL's implimentation of menu zeroing.

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,11 +1,13 @@
 #include "loginwindow.h"
+// #include "include/CryptoPP/secblock.h"
+#include "SecureString.cpp"
 #include "ui_loginwindow.h"
 #include <string>
 
-using secure_string = std::basic_string<char, std::char_traits<char>,
-                                        CryptoPP::AllocatorWithCleanup<char>>;
+// using secure_string = std::basic_string<char, std::char_traits<char>,
+//                                         CryptoPP::AllocatorWithCleanup<char>>;
 
-LoginWindow::LoginWindow(QWidget *parent)
+LoginWindow::LoginWindow(QWidget* parent)
     : QMainWindow(parent), ui(new Ui::LoginWindow) {
   ui->setupUi(this);
 }
@@ -14,12 +16,13 @@ LoginWindow::~LoginWindow() { delete ui; }
 
 void LoginWindow::on_lineEdit_2_returnPressed() {
   username = ui->lineEdit->text().toStdString();
-  secure_string password(ui->lineEdit_2->text().toStdString());
-  // TODO: Add database unlocking code
+  // secure_string password(ui->lineEdit_2->text().toStdString());
+  //  TODO: Add database unlocking code
+  data::string password(ui->lineEdit_2->text().toStdString());
 }
 
 void LoginWindow::on_pushButton_clicked() {
   username = ui->lineEdit->text().toStdString();
-  secure_string password(ui->lineEdit_2->text().toStdString());
-  // TODO: Add database unlocking code
+  // secure_string password(ui->lineEdit_2->text().toStdString());
+  //  TODO: Add database unlocking code
 }
--- loginwindow.h
+++ loginwindow.h
@@ -1,31 +1,31 @@
 #ifndef LOGINWINDOW_H
 #define LOGINWINDOW_H
 
-#include "include/CryptoPP/secblock.h"
+// #include "include/CryptoPP/secblock.h"
 #include <QMainWindow>
 
 namespace Ui {
-class LoginWindow;
+  class LoginWindow;
 }
 
 class LoginWindow : public QMainWindow {
   Q_OBJECT
 
-public:
-  explicit LoginWindow(QWidget *parent = nullptr);
+  public:
+  explicit LoginWindow(QWidget* parent = nullptr);
   ~LoginWindow();
-  using secure_string = std::basic_string<char, std::char_traits<char>,
-                                          CryptoPP::AllocatorWithCleanup<char>>;
+  // using secure_string = std::basic_string<char, std::char_traits<char>,
+  //                                         CryptoPP::AllocatorWithCleanup<char>>;
   std::string username;
-  secure_string password;
+  // secure_string password;
 
-private slots:
+  private slots:
   void on_lineEdit_2_returnPressed();
 
   void on_pushButton_clicked();
 
-private:
-  Ui::LoginWindow *ui;
+  private:
+  Ui::LoginWindow* ui;
 };
 
 #endif // LOGINWINDOW_H
