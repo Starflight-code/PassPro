a0926251b2d28e5fc130fad247bd27055af52507
ben-kobiske <kobiskeb@uwplatt.edu>
2023-11-30 01:48:51 -0600

    Enabled baseline viewing of all UI pages Enabled viewing of all UI pages. These pages are not yet fully functional and are for demonstration purposes only (as of now).

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,8 +3,34 @@
 
 MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
+  bool inClassDemo = true;
+
+  if (inClassDemo) {
+    ui->tableView->hide();
+  } else {
+  ui->columnView->hide();
+  ui->columnView_2->hide();
+  ui->columnView_3->hide();
+  ui->columnView_4->hide();
+  }
+
+  QStandardItemModel tableContent(2, 2);
+  for (int row = 0; row < tableContent.rowCount(); ++row) {
+    for (int column = 0; column < tableContent.columnCount(); ++column) {
+      QStandardItem *item = new QStandardItem(QString("row %0, column %1").arg(row).arg(column));
+      tableContent.setItem(row, column, item);
+    }
+  }
+  ui->tableView->setModel(&tableContent);
+  //ui->tableView->update();
 }
 
 MainWindow::~MainWindow() {
   delete ui;
 }
+
+void MainWindow::on_pushButton_clicked()
+{
+  entry.show();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QStandardItemModel>
+#include "entryviewer.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -15,8 +17,13 @@ class MainWindow : public QMainWindow {
   public:
   MainWindow(QWidget* parent = nullptr);
   ~MainWindow();
+  QStandardItemModel tableContent;
+
+  private slots:
+  void on_pushButton_clicked();
 
   private:
   Ui::MainWindow* ui;
+  EntryViewer entry;
 };
 #endif // MAINWINDOW_H
